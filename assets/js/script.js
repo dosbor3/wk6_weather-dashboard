f578250c638e1220ef352a0e20c86a1559753fcd
I am Billie <dosbor3@gmail.com>
2022-01-09 18:33:03 -0600

    updated index.html

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+/*
+*/
+
+//OpenWeather API key
+var apiKey = "8d4e7337b0329ab52081ed5c7aef9126";
+
+
+var getWeatherData = function(city) {   
+    var apiUrl ="api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    console.log(apiUrl);
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {  
+            response.json().then(function(data) {
+            console.log(data);
+        });
+        } 
+        else {
+            alert('Error: City Not Found');
+        }
+    })       
+        
+        .catch(function(error) {
+            
+            alert("Unable to connect to Open Weather");
+        });
+    };
+
+    getWeatherData("Atlanta");
